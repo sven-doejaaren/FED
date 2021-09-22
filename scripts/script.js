cd03ee0ecbd6c89dd9c0ea50a5505d41ad9cd158
sven-doejaaren <90143650+sven-doejaaren@users.noreply.github.com>
2021-09-22 21:17:51 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+var zoek = document.querySelector("input[type=search]");
+
+zoek.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        if (zoek.value === "ssx3") {
+            window.location.href = "detail.html";
+        } else {
+            console.log("boe");
+            // micro-interactie "Spel niet gevonden" tekst oid
+        }
+    }
+});
