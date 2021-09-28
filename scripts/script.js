1c83e0416beff618663f7dc176540985f2a4acb7
sven-doejaaren <90143650+sven-doejaaren@users.noreply.github.com>
2021-09-28 23:11:45 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,12 @@ var zoek = document.querySelector("input[type=search]");
 
 zoek.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
-        if (zoek.value === "ssx3") {
+        if (zoek.value === "ssx3" || zoek.value === "ssx 3" || zoek.value === "SSX3" || zoek.value === "SSX 3") {
             window.location.href = "detail.html";
         } else {
             console.log("boe");
             // micro-interactie "Spel niet gevonden" tekst oid
+            document.getElementsByName('search')[0].placeholder='Spel niet gevonden. Probeer opnieuw';
         }
     }
 });
